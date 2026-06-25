6dd0a044f7bbf40be822e719df21fa49219fa907
U2lu <ayoutulu@gmail.com>
2026-06-26 00:11:18 +0100

    added await, async and fetch functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,13 @@ const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeatherData = async (city: string) => {
-  
-}
+  const response = await fetch(
+    `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
+  );
 
-
-
+  const data = await response.json();
+  return data;
+};
 
 /*
 export const fetchWeatherData = async (city) => {
