5ccac7e25968bd7a05e29851a9466b136d02b100
U2lu <ayoutulu@gmail.com>
2026-06-27 00:45:09 +0100

    minor tweak

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeatherData = async (city: string) => {
   const response = await fetch(
-    `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`,
   );
 
   const data = await response.json();
