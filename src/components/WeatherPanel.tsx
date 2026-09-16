cb3cea9dc5c9b526b663c1db4f5ff4a98b728fb1
U2lu <ayoutulu@gmail.com>
2026-09-16 18:28:50 +0100

    for adding icons

--- src/components/WeatherPanel.tsx
+++ src/components/WeatherPanel.tsx
@@ -3,6 +3,7 @@ type WeatherPanelProps = {
   city: string;
   condition: string;
   date: string;
+  icon: string;
 };
 
 const WeatherPanel = ({
@@ -10,9 +11,11 @@ const WeatherPanel = ({
   temperature,
   date,
   condition,
+  icon,
 }: WeatherPanelProps) => {
   return (
     <div className=" h-full flex flex-col items-start justify-end gap-4 p-10">
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={condition} className="w-20 h-20" />
       <p className="text-7xl">{temperature}°</p>
       <p className="text-4xl">{city}</p>
       <p className="text-2xl">{condition}</p>
