ed40e9c5c7385d079d369f8e59dcf38d7c06d896
U2lu <ayoutulu@gmail.com>
2026-09-16 18:26:48 +0100

    API for the forecasting

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,29 @@
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-import type { WeatherData } from "../types/weather";
+const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
+import type { WeatherData, ForecastData } from "../types/weather";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   const response = await fetch(
     `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`,
   );
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
 
   const data = (await response.json()) as WeatherData;
+  return data;
+};
+
+export const getForecastData = async (city: string): Promise<ForecastData> => {
+  const response = await fetch(
+    `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`,
+  );
+  if (!response.ok) {
+    throw new Error("Could not fetch forecast");
+  }
+
+  const data = (await response.json()) as ForecastData;
   return data;
 };
 
