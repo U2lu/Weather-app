8ae9ce01031e19b2cd7036bfe5aaf849e6cd7535
U2lu <ayoutulu@gmail.com>
2026-09-18 17:29:55 +0100

    Worked on the Forecast List component, to start working

--- src/components/ForecastList.tsx
+++ src/components/ForecastList.tsx
@@ -1,7 +1,36 @@
-const ForecastList = () => {
-  return (
-    <div>ForecastList</div>
-  )
-}
+import type { ForecastData } from "../types/weather";
 
-export default ForecastList
+type ForecastListProps = {
+  forecastData: ForecastData | null;
+};
+
+const ForecastList = ({ forecastData }: ForecastListProps) => {
+  if (!forecastData) {
+    return <p>No forecast data available</p>;
+  }
+
+  const dailyForecast = forecastData.list.filter((item, index, array) => {
+    const date = item.dt_txt.split(" ")[0];
+
+    return (
+      index ===
+      array.findIndex((forecast) => forecast.dt_txt.split(" ")[0] === date)
+    );
+  });
+
+  return( 
+    <div>
+      <h2>5 Day Forecast</h2>
+
+      {dailyForecast.slice(0, 5).map((day) => (
+        <div key={day.dt}>
+          <p>{day.dt_txt.split(" ")[0]}</p>
+          <p>{day.main.temp}°C</p>
+          <p>{day.weather[0].main}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ForecastList;
